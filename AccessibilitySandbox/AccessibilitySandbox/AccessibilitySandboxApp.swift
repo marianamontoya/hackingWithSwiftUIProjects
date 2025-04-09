282fbb99187c9501aa7007cfbc964290400edb68
Mariana Montoya <marianamontoya1120@gmail.com>
2025-04-09 15:40:03 -0700

    Added project 15 on which is about accessibility

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Mariana Montoya on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -0,0 +1,85 @@
+//
+//  ContentView.swift
+//  AccessibilitySandbox
+//
+//  Created by Mariana Montoya on 4/9/25.
+//
+
+import SwiftUI
+
+//struct ContentView: View {
+//    let pictures = [
+//        "ales-krivec-15949@2x", "galina-n-189483@2x", "kevin-horstmann-141705@2x", "nicolas-tissot-335096@2x"
+//    ]
+//    
+//    let labels = [
+//        "Tulips",
+//        "Frozen tree buds",
+//        "Sunflowers",
+//        "Fireworks"
+//    ]
+//    
+//    @State private var selectedPicture = Int.random(in: 0...3)
+//    
+//    
+//    var body: some View {
+//        Button {
+//            selectedPicture = Int.random(in: 0...3)
+//        } label: {
+//            Image(pictures[selectedPicture])
+//                .resizable()
+//                .scaledToFit()
+//                .onTapGesture {
+//                    selectedPicture = Int.random(in: 0...3)
+//                }
+//        }
+//        .accessibilityLabel(labels[selectedPicture])
+//    }
+//}
+
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Text("Your score is")
+//            
+//            Text("1000")
+//                .font(.title)
+//        }
+//        .accessibilityElement(children: .ignore)
+//        .accessibilityLabel("Your score is 1000")
+//        
+//    }
+//}
+
+struct ContentView: View {
+    @State private var value = 10
+    var body: some View {
+        VStack {
+            Text("Value: \(value)")
+            
+            Button("Increment") {
+                value += 1
+            }
+            Button("Decrement") {
+                value -= 1
+            }
+        }
+        .accessibilityElement()
+        .accessibilityLabel("Value")
+        .accessibilityValue(String(value))
+        .accessibilityAdjustableAction{ direction in
+            switch direction{
+            case .increment:
+                value += 1
+            case .decrement:
+                value -= 1
+            default:
+                print("Not handled")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
